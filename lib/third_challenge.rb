19c24b936c50616014d04b41be85872ca6a96a58
Skye Geraghty <skyetay03@googlemail.com>
2020-01-24 17:43:52 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,24 +3,37 @@ def third_challenge
    montague: {
       patriarch: {
         #your key/value pairs here
+        name: ""
+        age: ""
         },
       matriarch: {
         #your key/value pairs here
+        name: ""
+        age: ""
+        
         },
       hero: {
         #your key/value pairs here
+        name: ""
+        age: ""
         },
       hero_friends: []
    },
    capulet: {
       patriarch: {
         #your key/value pairs here
+        name: ""
+        age: ""
         },
       matriarch: {
         #your key/value pairs here
+        name: ""
+        age: ""
         },
       heroine: {
         #your key/value pairs here
+        name: ""
+        age: ""
         },
       heroine_friends: []
     }
